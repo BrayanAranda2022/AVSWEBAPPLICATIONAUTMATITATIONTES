d7988023797c7d8b865f54438afd23e111b337cd
Brayan Eduardo Camacho Pacheco <brayan.camacho@arandasoft.com>
2022-03-11 15:19:28 -0500

    ajustes generales

---
+++ Aranda.Avs.Automatization.WebApplication.Test/StepDefinitions/LoginStepDefinitions.cs
@@ -0,0 +1,33 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Reflection;
+using TechTalk.SpecFlow;
+
+namespace Aranda.Avs.Automatization.WebApplication.Test.StepDefinitions
+{
+    [Binding]
+    public class LoginStepDefinitions
+    {
+        [Given(@"\[context]")]
+        public void GivenContext()
+        {
+            var router = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string route = Path.Combine(router, "Drivers");
+            IWebDriver webDriver = new ChromeDriver(route);
+            webDriver.Navigate().GoToUrl("http://avs-web-dev.azurewebsites.net/");
+            webDriver.Manage().Window.Maximize();
+        }
+
+        [When(@"\[action]")]
+        public void WhenAction()
+        {
+         
+
+        [Then(@"\[outcome]")]
+        public void ThenOutcome()
+        {
+           
+        }
+    }
+}
